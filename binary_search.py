1830871b6572f1ad17831b2350e93c878e541330
Relativiteit <alejocain@gmail.com>
2024-12-21 19:20:14 +0900

    added randint to make it a little bit different

--- binary_search.py
+++ binary_search.py
@@ -1,4 +1,6 @@
 import random
+
+
 array_1 = [1, 2, 4, 6, 8, 10]
 array_2 = [2, 4, 6, 7, 9]
 
@@ -12,13 +14,14 @@ def binary_search(list, item):
         guess = list[mid]
         if guess == item:
             print(mid)
+            print(item)
             return mid
         if guess > item:
             high = mid - 1
         else:
             low = mid + 1
-    return print("not in collection")
+    return print("not in collection", item)
 
 
-binary_search(array_2, )
-binary_search(array_1, 2)
+binary_search(array_2, random.randint(1, 9))
+binary_search(array_1, random.randint(1, 9))
